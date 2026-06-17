5bf80a0d04f4df71cf645512f56b9df4b0be4f03
asus <asus@LAPTOP-H6PM25FQ>
2026-06-16 20:36:21 -0600

    feat: agregar CitasApp.Api con endpoints REST

---
+++ CitasApp.Api/Program.cs
@@ -0,0 +1,33 @@
+﻿using CitasApp.Application.Services;
+using CitasApp.Domain.Interfaces;
+using CitasApp.Infrastructure.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Rutas de archivos JSON
+var dataFolder = Path.Combine(Directory.GetCurrentDirectory(), "data");
+Directory.CreateDirectory(dataFolder);
+
+var jsonPacientes = Path.Combine(dataFolder, "pacientes.json");
+var jsonMedicos = Path.Combine(dataFolder, "medicos.json");
+var jsonCitas = Path.Combine(dataFolder, "citas.json");
+
+// Repositorios
+builder.Services.AddScoped<IPacienteRepository>(_ => new JsonPacienteRepository(jsonPacientes));
+builder.Services.AddScoped<IMedicoRepository>(_ => new JsonMedicoRepository(jsonMedicos));
+builder.Services.AddScoped<ICitaRepository>(_ => new JsonCitaRepository(jsonCitas));
+
+// Servicios de aplicación
+builder.Services.AddScoped<PacienteService>();
+builder.Services.AddScoped<MedicoService>();
+builder.Services.AddScoped<CitaService>();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
