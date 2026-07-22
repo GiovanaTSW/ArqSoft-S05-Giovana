90301faadf87ae268805ece96153c4920b927f82
asus <asus@LAPTOP-H6PM25FQ>
2026-07-22 14:02:33 -0600

    Agrega pruebas xUnit para CsvCitaRepository

---
+++ CitasApp.Tests/CsvCitaRepositoryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using CitasApp.Infrastructure.Repositories;
+
+namespace CitasApp.Tests
+{
+    public class CsvCitaRepositoryTests
+    {
+        [Fact]
+        public void TestCrearCsvCitaRepository_RetornaInstanciaValida()
+        {
+            // Arrange & Act
+            var repo = new CsvCitaRepository("citas.csv");
+
+            // Assert
+            Assert.NotNull(repo);
+        }
+    }
+}
