d35e40093d508c7dcbdc298853a73d602a39e659
asus <asus@LAPTOP-H6PM25FQ>
2026-07-22 14:03:57 -0600

    Agrega pruebas xUnit para MedicoModel

---
+++ CitasApp.Tests/MedicoModelTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using CitasApp.Domain.Models;
+
+namespace CitasApp.Tests
+{
+    public class MedicoModelTests
+    {
+        [Fact]
+        public void AsignarNombre_Medico_GuardaCorrectamente()
+        {
+            // Arrange
+            var medico = new Medico();
+
+            // Act
+            medico.Nombre = "Dra. Smith";
+
+            // Assert
+            Assert.Equal("Dra. Smith", medico.Nombre);
+        }
+    }
+}
