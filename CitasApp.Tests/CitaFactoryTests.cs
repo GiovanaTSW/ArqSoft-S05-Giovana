39cebcb7aa63b53fb28445cb61797664c40b47a0
asus <asus@LAPTOP-H6PM25FQ>
2026-07-22 13:58:52 -0600

    Agrega pruebas xUnit y corrige configuracion

---
+++ CitasApp.Tests/CitaFactoryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using CitasApp.Infrastructure.Repositories;
+
+namespace CitasApp.Tests
+{
+    public class CitaFactoryTests
+    {
+        [Fact]
+        public void TestCrearRepositorioJson_RetornaInstanciaValida()
+        {
+            // Arrange & Act
+            var repo = new JsonCitaRepository("citas.json");
+
+            // Assert
+            Assert.NotNull(repo);
+        }
+    }
+}
